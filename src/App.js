d6979fc7492d967a0bbeb05f4fab8ca3c5f4ac0e
Sajal Sirohi <sajal.konfhub@gmail.com>
2020-06-02 20:10:44 +0530

    started routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => {
+  return (
+    <h1> Hats Page!!</h1>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+    <BrowserRouter>
+    <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
+    </BrowserRouter>
+ 
     </div>
   );
 }
