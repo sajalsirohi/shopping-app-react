94ac73a1f0c0514620ebbb2452e0be727a842dee
Sajal Sirohi <sajal.konfhub@gmail.com>
2020-06-06 14:35:01 +0530

    added initial selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import cartItem from "../cart-item/cart-item.component";
 import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
+
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
@@ -11,16 +12,16 @@ import CartItem from "../cart-item/cart-item.component";
 const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {cartItems.map((cartItem) => (
-      <CartItem key={cartItem.id} item={cartItem} />
-    ))}
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
