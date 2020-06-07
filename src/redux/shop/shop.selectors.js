8c9ec42d934fc98b71ae91f200b652ec2180118f
Sajal Sirohi <sajal.konfhub@gmail.com>
2020-06-07 17:20:00 +0530

    moved direct and shop into own reducer

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,14 @@ export const selectCollection = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionForPreview = createSelector(
+  [selectCollection],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectACollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollection],
+    (collections) => collections[collectionUrlParam]
+  );
